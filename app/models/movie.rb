31edfa8aab71adf288cc80ce39ee81bf092a8d47
Allyson Halley <isolation85@gmail.com>
2019-04-23 00:24:41 -0300

    test in movie_controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
+
   belongs_to :category
+
+  # Essa validação foi inserida após acusação no teste do controller
+  has_many :evaluations, dependent: :delete_all
+  validates :name, :summary, :year, :category, presence: true
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -4,4 +4,24 @@ class MovieTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should be invalid without a title" do
+    movies(:one).name = nil
+    assert_nil movies(:one).name
+    assert_equal false, movies(:one).valid?
+  end
+  test "should be invalid without a summary" do
+    movies(:one).summary = nil
+    assert_nil movies(:one).summary
+    assert_equal false, movies(:one).valid?
+  end
+  test "should be invalid without a year" do
+    movies(:one).year = nil
+    assert_nil movies(:one).year
+    assert_equal false, movies(:one).valid?
+  end
+  test "should be invalid without a category" do
+    movies(:one).category = nil
+    assert_nil movies(:one).category
+    assert_equal false, movies(:one).valid?
+  end
 end
